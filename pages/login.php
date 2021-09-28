cec6f5f76b9e7c2a339db2a0997b329c55d46422
RalkeyOfficial <Ralkey@outlook.com>
2021-09-28 14:34:34 +0200

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -5,9 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/main-style.css">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
-    
+    <form action="../includes/login.inc.php">
+        <input type="text" name="username" id="username">
+    </form>
 </body>
 </html>
