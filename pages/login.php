9adb44dd4252e114d8705c24ac3171bcc36905e1
RalkeyOfficial <Ralkey@outlook.com>
2021-09-28 15:27:13 +0200

    worked on the CSS

--- pages/login.php
+++ pages/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style/main-style.css">
+    <link rel="stylesheet" href="../style/login-style.css">
     <title>Login</title>
 </head>
 <body>
