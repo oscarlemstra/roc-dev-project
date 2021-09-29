405356e2befe519dd933f4522f187c0b7c38c209
RalkeyOfficial <Ralkey@outlook.com>
2021-09-29 09:24:43 +0200

    added font and finished CSS

--- pages/login/index.php
+++ pages/login/index.php
@@ -4,18 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style/login-style.css">
+    <link rel="stylesheet" href="../../style/login-style.css">
     <title>Login</title>
 </head>
 <body>
     <div class="container">
-        <form action="../includes/login.inc.php">
-            <label for="email">Email adress:</label>
-            <input type="email" name="email" id="email">
+        <form action="../../includes/login.inc.php">
+            <h1>Inloggen</h1>
 
-            <label for="password">Wachtwoord:</label>
-            <input type="password" name="password" id="password">
-
+            <input type="email" name="email" placeholder="Email">
+            <input type="password" name="password" placeholder="Wachtwoord">
             <input type="submit" value="submit">
         </form>
     </div>
