10438c1d3690c58dc9f50e5530247914c469e9fd
RalkeyOfficial <Ralkey@outlook.com>
2021-09-29 09:25:34 +0200

    renamed files

--- pages/login/index.php
+++ pages/login/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../style/login-style.css">
+    <link rel="stylesheet" href="../../style/login-register-style.css">
     <title>Login</title>
 </head>
 <body>
