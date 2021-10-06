314251cd30af9c6ad2cd123d0f9026072b3978db
oscarlemstra <61353780+oscarlemstra@users.noreply.github.com>
2021-10-06 12:43:03 +0200

    Update signup_handler.php

--- php/signup_handler.php
+++ php/signup_handler.php
@@ -19,6 +19,9 @@
 // 
 // =================================================================== //
 
+require_once('../includes/DatabaseManager.php');
+$dbm = new DatabaseManager();
+
 // a .inc.php file is a file that is put inside the includes folder
 // includes are .php files that are require'd in another file. such as this one
 require_once('../includes/signup_error_handling.php');
@@ -37,13 +40,20 @@ $confirmpwd = $_POST['confirmPassword'];
 // if the functions returns false
 //      continue with the rest of the code
 $result = emailCheck($email, $confirmEmail);
-if ( $result ) {
+if ($result) {
     $result = str_replace(" ", "%20", $result);
-    header('location: ../../../pages/signup/?error=' . $result);
+    header('location: ../pages/signup/?error=' . $result);
 }
 
+
 $result = pwdCheck($pwd, $confirmpwd, $email);
-if ( $result ) {
+if ($result) {
     $result = str_replace(" ", "%20", $result);
-    header('location: ../../../pages/signup/?error=' . $result);
+    header('location: ../pages/signup/?error=' . $result);
 }
+
+
+//$hashedPassword = hash("sha3-512", $_POST['password']);
+//$dbm->insertRecordToUser("1", $_POST['email'], $hashedPassword);
+
+//header('location: ../pages/login');
