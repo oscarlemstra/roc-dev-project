8506c702fef6a25abf8c079160160a508340ce43
oscarlemstra <61353780+oscarlemstra@users.noreply.github.com>
2021-10-12 15:52:29 +0200

    Update verification_handler.php

--- php/verification_handler.php
+++ php/verification_handler.php
@@ -15,8 +15,18 @@ if ($result) {
     header('location: ../pages/email-verification');
     exit();
 }*/
+$headers = 'From: 2026970@talnet.nl' . "\r\n" .
+    'Reply-To: olemstra@roc-dev.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
 
-echo mail("olemstra@roc-dev.com", "test", "wow het werkt");
+$result = mail("olemstra@roc-dev.nl", "test", "wow het werkt", $headers);
+
+if ($result) {
+    echo "email is verzonden";
+}
+else {
+    echo "email is niet verzonden";
+}
 
 //$hashedPassword = hash("sha3-512", $_POST['password']);
 //$dbm->insertRecordToUser("1", $_POST['email'], $hashedPassword);
