9b03bedd12711d9d4446210b334b2d5f0d394e3d
Koen <2144264@talnet.nl>
2021-10-13 15:00:30 +0200

    totp login directory

---
+++ pages/totp-login/index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>totp</title>
+</head>
+<body>
+<form action="index.php" method="post">
+    <input type="text" name="pass-code">
+    <button type="submit" name="submit">CLICK!</button>
+</form>
+</body>
+</html>
