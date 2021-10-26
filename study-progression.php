21a83aaf66c8d6e9837330cc2df78e9b87330fb6
ZoruaStar64 <sanderb119@gmail.com>
2021-10-26 10:29:12 +0200

    start of study-progression branch
    made css docs
    made universal css (colors/padding/margin/box-sizing)
    made study-progression file (just the start nothing special yet)

---
+++ study-progression.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="" /> <!--needs a favicon here (roc-dev logo or software dev opleiding logo)-->
+    <link rel="stylesheet" href="styles/universal.css">
+    <title>Study Progression</title>
+</head>
+<body>
+<div class="container">
+
+</div>
+</body>
+</html>
+
