428d0809c0b8ad1cbfaab30f1c0be435cb81e610
ZoruaStar64 <sanderb119@gmail.com>
2021-10-26 13:48:40 +0200

    added hamburger and start on top of page (mobile)

--- study-progression.php
+++ study-progression.php
@@ -9,12 +9,18 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="" /> <!--needs a favicon here (roc-dev logo or software dev opleiding logo)-->
+    <link rel="icon" href="images/02_ROCvF_logo_PLAT_RGB%201.png" /> <!--needs a favicon here (roc-dev logo or software dev opleiding logo)-->
     <link rel="stylesheet" href="styles/study-progression.css">
     <title>Study Progression</title>
 </head>
 <body>
 <div class="container">
+<div class="header color-roc-orange">
+    <img class="logo" alt="ROC Logo" src="images/02_ROCvF_logo_PLAT_RGB%201.png">
+
+
+</div>
+
 
 </div>
 </body>
