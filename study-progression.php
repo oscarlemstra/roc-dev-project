e6491f6cfdb36ae8763f1ecc80ca78523686bf5e
ZoruaStar64 <sanderb119@gmail.com>
2021-10-27 11:18:14 +0200

    deleted unrelated things + worked on subject container template

--- study-progression.php
+++ study-progression.php
@@ -23,14 +23,21 @@ session_start();
 
     <div class="planning">
         <h2 class="color-roc-orange-text">Planning van vandaag</h2>
-        <h3>Datum: <?php //hier komt de datum van de planning wat er nu staat is gewoon testtext ?> 27/10/2021</h3>
+        <h3>Datum: <?php //hier komt de datum van de planning. wat er nu staat is gewoon testtext ?> 27/10/2021</h3>
         <br>
         <?php //dit is het punt dat we de tickets uit de database zouden moeten halen
               //en dan het op een rijtje onderelkaar echo-en
-              //dus bvb: <tr>ticket 1</tr> <tr>ticket 2</tr> etc.
+              //dus bvbmet een foreach die elke vak als een div echo-ed
               //als er teveel ruimte is echo maar een <br> erbij of voeg wat padding/margin toe   ?>
         <p>Rekenen - H1 - 3 pnt</p><p>Databases ontwerpen - DeeBeeTrain - 6pnt</p><p>roc-dev-project - study progression - 6pnt</p>
         <a href="#"><div class="color-roc-orange planning-create-button"><p class="text-align-middle">Edit/Create Planning</p></div></a>
+    </div>
+
+    <div class="subjects">
+    <!--dit is een template voor de vakcontainer-->
+        <div class="subject-container"><div class="subject-color-container color-kerntaak-frontend"></div><h2 class="subject-text-center">HTML</h2></div>
+        <div class="subject-container"></div>
+
     </div>
 
 </div>
