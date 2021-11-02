50b1b70ac2932167a258ee092ed71f9c41f0b178
Kenny Meijer <kenny014@hotmail.nl>
2021-11-02 13:33:09 +0100

    update

--- pages/study-progression/index.php
+++ pages/study-progression/index.php
@@ -9,16 +9,16 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="../images/02_ROCvF_logo_PLAT_RGB%201.png" /> <!--needs a favicon here (roc-dev logo or software dev opleiding logo)-->
-    <link rel="stylesheet" href="../styles/study-progression.css">
+    <link rel="icon" href="../../images/02_ROCvF_logo_PLAT_RGB%201.png" /> <!--needs a favicon here (roc-dev logo or software dev opleiding logo)-->
+    <link rel="stylesheet" href="../../styles/study-progression.css">
     <title>Study Progression</title>
 </head>
 <body>
 <div class="container">
 
 <div class="header color-roc-orange">
-    <img class="logo" alt="ROC Logo" src="../images/02_ROCvF_logo_PLAT_RGB%201.png">
-    <img class="hamburger" alt="hamburger" src="../images/hamburger.png">
+    <img class="logo" alt="ROC Logo" src="../../images/02_ROCvF_logo_PLAT_RGB%201.png">
+    <img class="hamburger" alt="hamburger" src="../../images/hamburger.png">
 </div>
 
     <div class="hamburger-menu color-roc-orange">
@@ -104,7 +104,7 @@ session_start();
     </div>
 
 </div>
-<script src="../javascript/study-progression.js"></script>
+<script src="../../javascript/study-progression.js"></script>
 </body>
 </html>
 
