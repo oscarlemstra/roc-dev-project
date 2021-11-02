6093bd88489969559fed8ef7607a1dc508c2ca75
ZoruaStar64 <sanderb119@gmail.com>
2021-11-02 13:41:58 +0100

    changed styles folder to style

--- pages/study-progression/index.php
+++ pages/study-progression/index.php
@@ -10,7 +10,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="../../images/02_ROCvF_logo_PLAT_RGB%201.png" /> <!--needs a favicon here (roc-dev logo or software dev opleiding logo)-->
-    <link rel="stylesheet" href="../../styles/mobile.css">
+    <link rel="stylesheet" href="../../style/mobile.css">
     <title>Study Progression</title>
 </head>
 <body>
