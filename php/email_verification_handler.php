14cecd7a7fed0ff60de83839b9ad7562a101df9b
oscarlemstra <61353780+oscarlemstra@users.noreply.github.com>
2021-11-03 14:59:19 +0100

    Update email_verification_handler.php

--- php/email_verification_handler.php
+++ php/email_verification_handler.php
@@ -1,3 +1,17 @@
-<?php
+<?php session_start();
 
-echo "test";
+require_once '../includes/DatabaseManager.php';
+$dbm = new DatabaseManager();
+
+$record = $dbm->getRecordsFromTable("email_verification_code", "email", $_SESSION['signup']['email']);
+
+if ($_POST['verification_code'] === $record[0]['code']) {
+    $dbm->deleteRecordsFromTable("email_verification_code", "email", $_SESSION['signup']['email']);
+    header('location: ../pages/study-progression');
+    exit();
+}
+else {
+    $_SESSION['errorMessage'] = "code is fout";
+    header('location: ../pages/6code-verification');
+    exit();
+}
