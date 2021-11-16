3c4afadc9fe1ba8cf30695260010ebcbdf22537f
Koen <2144264@talnet.nl>
2021-11-16 09:03:02 +0100

    make account WIP

--- php/make_account.php
+++ php/make_account.php
@@ -1,9 +1,34 @@
 <?php session_start();
+$_SESSION['signup']['user_role_id'] = 1;
+$_SESSION['signup']['group_id'] = 1;
+$_SESSION['signup']['student_nr'] = "1234567";
+$_SESSION['signup']['first_name'] = "henk";
+$_SESSION['signup']['tussenvoegsel'] = "van der";
+$_SESSION['signup']['last_name'] = "laan";
+$_SESSION['signup']['email'] = "henk.vanderlaan@student.rocvf.nl";
+$_SESSION['signup']['password'] = "sldkjfslkfjslkfjsflksjdflksdjflksdjflskdfjsldkfjsdlkfjsdlfksdjflksdjflskdjfsldkfjsdlkfjsldkfjsdlkfjsdllskdfjslkdfjlskdfjlskdffff";
+$_SESSION['signup']['secret'] = "slkfjsdlkfj";
 
-require_once '../includes/DatabaseManager.php';
+require_once "../includes/DatabaseManager.php";
+
+//connections
 $dbm = new DatabaseManager();
 
-//testing code
-print_r($dbm->checkConnectionToDatabase());
+//prepare insert array
+$insertArray = [
+    "user_role_id" => $_SESSION['signup']['user_role_id'],
+    "group_id" => $_SESSION['signup']['group_id'],
+    "student_nr" => $_SESSION['signup']['student_nr'],
+    "first_name" => $_SESSION['signup']['first_name'],
+    "tussenvoegsel" => $_SESSION['signup']['tussenvoegsel'],
+    "last_name" => $_SESSION['signup']['last_name'],
+    "email" => $_SESSION['signup']['email'],
+    "password" => $_SESSION['signup']['password'],
+    "secret" => $_SESSION['signup']['secret']
+];
 
-echo "wow";
+//insert into user table
+$dbm->insertRecordToTable("user", $insertArray);
+
+//go to homepage
+//header('location: ../pages/home');
