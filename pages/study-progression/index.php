3381cd81a993a64709ae3c473827617539da9439
Kenny Meijer <kenny014@hotmail.nl>
2021-11-17 08:38:06 +0100

    changed subjects to flex instead of grid

--- pages/study-progression/index.php
+++ pages/study-progression/index.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-if(!$_SESSION['logged_in']) {
-    header("location: ../login");
-    exit();
-}
+// if(!$_SESSION['logged_in']) {
+//     header("location: ../login");
+//     exit();
+// }
 
 
 
